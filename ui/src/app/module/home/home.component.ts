40a643ea92f33cbcb99f0d6e0b0593ade1e4ae5a
Rajkumar <rkvns0015@gmail.com>
2024-04-10 07:25:25 +0530

    yes

--- ui/src/app/module/home/home.component.ts
+++ ui/src/app/module/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GoogleChartInterface, GoogleChartType } from 'ng2-google-charts';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  public pieChart: GoogleChartInterface = {
+    chartType: GoogleChartType.PieChart,
+    dataTable: [
+      ['Task', 'Hours per Day'],
+      ['Work',     11],
+      ['Eat',      2],
+      ['Commute',  2],
+      ['Watch TV', 2],
+      ['Sleep',    7]
+    ],
+    //firstRowIsData: true,
+    options: {'title': 'Tasks'},
+  };
+  
 }
--- ui/src/app/module/home/home.module.ts
+++ ui/src/app/module/home/home.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import {HomeRoutingModule} from './home-routing.module';
-
-
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    Ng2GoogleChartsModule
   ],
   bootstrap: [HomeComponent]
 })
